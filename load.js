0ccb4b00b23406cfb22269cef9948d75a1a9b477
John Smith <awesomebing1Git@outlook.com>
2015-02-06 21:00:33 -0600

    Merp

--- load.js
+++ load.js
@@ -4,26 +4,25 @@ function renderProgress(progress)
 
     if(progress<25){
         var angle = -90 + (progress/100)*360;
-        document.getElementsByClassName('.animate-0-25-b').transform = "rotate(\""+angle+"\")";
+        document.getElementByClassName('.animate-0-25-b').transform = "rotate(\""+angle+"\")";
     }
     else if(progress>=25 && progress<50){
         var angle = -90 + ((progress-25)/100)*360;
-        document.getElementsByClassName('.animate-0-25-b').transform = "rotate(\"0deg\")";
-        document.getElementsByClassName('.animate-25-50-b').transform = "rotate(\""+angle+"\")";
+        document.getElementByClassName('.animate-0-25-b').transform = "rotate(\"0deg\")";
+        document.getElementByClassName('.animate-25-50-b').transform = "rotate(\""+angle+"\")";
     }
     else if(progress>=50 && progress<75){
         var angle = -90 + ((progress-50)/100)*360;
-        document.getElementsByClassName('.animate-25-50-b').transform = "rotate(\"0deg\")";
-        document.getElementsByClassName('.animate-0-25-b').transform = "rotate(\"0deg\")";
-        document.getElementsByClassName('.animate-50-75-b').transform = "rotate(\""+angle+"\")";
+        document.getElementByClassName('.animate-25-50-b').transform = "rotate(\"0deg\")";
+        document.getElementByClassName('.animate-0-25-b').transform = "rotate(\"0deg\")";
+        document.getElementByClassName('.animate-50-75-b').transform = "rotate(\""+angle+"\")";
     }
     else if(progress>=75 && progress<=100){
         var angle = -90 + ((progress-75)/100)*360;
-        $(".animate-50-75-b, .animate-25-50-b, .animate-0-25-b").css("transform","rotate(0deg)");
-        document.getElementsByClassName('.animate-0-25-b').transform = "rotate(\"0deg\")";
-        document.getElementsByClassName('.animate-25-50-b').transform = "rotate(\"0deg\")";
-        document.getElementsByClassName('.animate-50-75-b').transform = "rotate(\"0deg\")";
-        document.getElementsByClassName('.animate-75-100-b').transform = "rotate(\""+angle+"\")";
+        document.getElementByClassName('.animate-0-25-b').transform = "rotate(\"0deg\")";
+        document.getElementByClassName('.animate-25-50-b').transform = "rotate(\"0deg\")";
+        document.getElementByClassName('.animate-50-75-b').transform = "rotate(\"0deg\")";
+        document.getElementByClassName('.animate-75-100-b').transform = "rotate(\""+angle+"\")";
     }
     if(progress==100){
 
@@ -33,10 +32,10 @@ function renderProgress(progress)
 
 function clearProgress()
 {
-  document.getElementsByClassName('.animate-0-25-b').transform = "rotate(\"90deg\")";
-  document.getElementsByClassName('.animate-25-50-b').transform = "rotate(\"90deg\")";
-  document.getElementsByClassName('.animate-50-75-b').transform = "rotate(\"90deg\")";
-  document.getElementsByClassName('.animate-75-100-b').transform = "rotate(\"90deg\")";
+  document.getElementByClassName('.animate-0-25-b').transform = "rotate(\"90deg\")";
+  document.getElementByClassName('.animate-25-50-b').transform = "rotate(\"90deg\")";
+  document.getElementByClassName('.animate-50-75-b').transform = "rotate(\"90deg\")";
+  document.getElementByClassName('.animate-75-100-b').transform = "rotate(\"90deg\")";
 }
 
 var i=0;
