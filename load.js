59894a2166c304f4b76ebfdb0f000476f8fc5147
John Smith <awesomebing1Git@outlook.com>
2015-02-06 20:54:59 -0600

    Convert to javascript
    It was jQuery

--- load.js
+++ load.js
@@ -4,32 +4,39 @@ function renderProgress(progress)
 
     if(progress<25){
         var angle = -90 + (progress/100)*360;
-        $(".animate-0-25-b").css("transform","rotate("+angle+"deg)");
+        document.getElementsByClassName('.animate-0-25-b').transform = "rotate(\""+angle+"\")";
     }
     else if(progress>=25 && progress<50){
         var angle = -90 + ((progress-25)/100)*360;
-        $(".animate-0-25-b").css("transform","rotate(0deg)");
-        $(".animate-25-50-b").css("transform","rotate("+angle+"deg)");
+        document.getElementsByClassName('.animate-0-25-b').transform = "rotate(\"0deg\")";
+        document.getElementsByClassName('.animate-25-50-b').transform = "rotate(\""+angle+"\")";
     }
     else if(progress>=50 && progress<75){
         var angle = -90 + ((progress-50)/100)*360;
-        $(".animate-25-50-b, .animate-0-25-b").css("transform","rotate(0deg)");
-        $(".animate-50-75-b").css("transform","rotate("+angle+"deg)");
+        document.getElementsByClassName('.animate-25-50-b').transform = "rotate(\"0deg\")";
+        document.getElementsByClassName('.animate-0-25-b').transform = "rotate(\"0deg\")";
+        document.getElementsByClassName('.animate-50-75-b').transform = "rotate(\""+angle+"\")";
     }
     else if(progress>=75 && progress<=100){
         var angle = -90 + ((progress-75)/100)*360;
         $(".animate-50-75-b, .animate-25-50-b, .animate-0-25-b").css("transform","rotate(0deg)");
-        $(".animate-75-100-b").css("transform","rotate("+angle+"deg)");
+        document.getElementsByClassName('.animate-0-25-b').transform = "rotate(\"0deg\")";
+        document.getElementsByClassName('.animate-25-50-b').transform = "rotate(\"0deg\")";
+        document.getElementsByClassName('.animate-50-75-b').transform = "rotate(\"0deg\")";
+        document.getElementsByClassName('.animate-75-100-b').transform = "rotate(\""+angle+"\")";
     }
     if(progress==100){
 
     }
-    $(".text").html(progress+"%");
+    document.getElementByClassName(".text").innerHTML = progress+"%";
 }
 
 function clearProgress()
 {
-    $(".animate-75-100-b, .animate-50-75-b, .animate-25-50-b, .animate-0-25-b").css("transform","rotate(90deg)");
+  document.getElementsByClassName('.animate-0-25-b').transform = "rotate(\"90deg\")";
+  document.getElementsByClassName('.animate-25-50-b').transform = "rotate(\"90deg\")";
+  document.getElementsByClassName('.animate-50-75-b').transform = "rotate(\"90deg\")";
+  document.getElementsByClassName('.animate-75-100-b').transform = "rotate(\"90deg\")";
 }
 
 var i=0;
@@ -40,4 +47,4 @@ setInterval(function (){
                     clearProgress();
                 }
                 renderProgress(i);
-}, 200);  
+}, 200);
