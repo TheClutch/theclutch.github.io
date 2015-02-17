a6e6ba6e863d103c598bc5ac12c72a6f7cc23d9c
John Smith <awesomebing1@users.noreply.github.com>
2015-02-17 14:55:08 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 function setName(name) {
   $('.title').html = name;
 }
+function beginShow() {
 setInterval(setName('ClutchNightmare'), 3000);
 setInterval(setName("- and -"), 3000);
 setInterval(setName('awesomebing1'), 3000);
 $(".avatar").attr('src', 'https://minotar.net/avatar/awesomebing1.png');
+}
