eff0f284225250ab93ef1026fc4689f8ba263d0b
John Smith <awesomebing1@users.noreply.github.com>
2015-02-18 14:38:56 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 function one() {
   $('.title').text('ClutchNightmare');
-  setInterval(two, 3000);
+  setTimeout(function() {
+      setTimeout(function() {
+        $('.title').text("- and -");
+      setTimeout(function() {
+        $('.title').text("awesomebing1");
+  $(".avatar").attr('src', 'https://minotar.net/avatar/awesomebing1.png');
+}, 3000);
+}, 3000);
+}, 3000);
 }
 function two() {
-  $('.title').text("- and -");
+  
   setInterval(three, 3000);
 }
 function three() {
-  $('.title').text("awesomebing1");
-  $(".avatar").attr('src', 'https://minotar.net/avatar/awesomebing1.png');
+
   setInterval(taDa, 3000);
 }
 function taDa() {
